b080980c07d1d6947963ca3de16a11ad4be7429b
PaoNoEyes <kieran.gelly@gmail.com>
2025-07-09 09:51:48 +0200

    Ajout exo 4 et corrections

--- exos copain/python/Ex_2 python.py
+++ exos copain/python/Ex_2 python.py
@@ -1,14 +1,11 @@
 #Ex_2 python
+#a=["fraise","pomme","poire","pomme","banane","abricot","fraise","mure","fraise","cerise","peche","pasteque","abricot","melon","cerise","framboise","fraise","pamplemousse","abricot","orange","clementine","ananas","kiwi","easteregg","pomme","ananas","pomme","poire","fraise","fraise","poire","pomme","myrtille","cassis","salade","tomate","avocat","fraise","framboise","ananas","kiwi","orange","poire","pamplemousse","clémentine","pomme","poire","clémentine","ananas","clémentine","pomme","poire","pomme","fraise"]
 
 
-a=["fraise","pomme","poire","pomme","banane","abricot","fraise","mure","fraise","cerise","peche","pasteque","abricot","melon","cerise","framboise","fraise","pamplemousse","abricot","orange","clementine","ananas","kiwi","easteregg","pomme","ananas","pomme","poire","fraise","fraise","poire","pomme","myrtille","cassis","salade","tomate","avocat","fraise","framboise","ananas","kiwi","orange","poire","pamplemousse","clémentine","pomme","poire","clémentine","ananas","clémentine","pomme","poire","pomme","fraise"]
 result={"fraise":6}
 ok = False
-
-
 def fonc1(a,b = "fraise"):
     for x in a :
-
         if x in result:
             ok = False
             result[x] = result[x]+1
@@ -16,7 +13,6 @@ def fonc1(a,b = "fraise"):
             ok = True
             result[x] = 1
     return result
-
 print(fonc1(a))
 
         
